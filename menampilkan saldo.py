ed54467d1596803426aceb4ede85e5ddd299cecd
Namhal <rahman.ak@gmail.com>
2025-11-14 22:25:43 +0800

    nambah foto

---
+++ menampilkan saldo.py
@@ -0,0 +1,17 @@
+# menampilkan saldo club
+# selalu bertmabah
+#  duit barca, 120000000
+
+import time
+import threading
+from data import data_barcelona
+
+
+def saldo_barca():
+    global saldo
+    while True:
+
+        return
+
+
+thr = threading.Thread(target=saldo_barca, args=data["saldo"])
